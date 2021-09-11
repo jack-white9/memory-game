cc77ace543f7cae053bfc62da50f591e77de272a
Jack White <jfw793@gmail.com>
2021-09-11 17:07:31 +1000

    Implemented random card feature

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../themes/Cards.css';
 
 const Cards = ({setScore}) => {
+    const cardArray = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+    let updatedArray = [1,2,3,4,5,6,7,8,9,10,11,12];
+    useEffect(function randomiseCards() {
+        updatedArray = [];
+        for (let i = 0; i < 12; i++) {
+            let randomNum = parseInt(Math.random() * 12);
+            if (updatedArray.includes(cardArray[randomNum])) {
+                i -= 1;
+            } else {
+                updatedArray.push(cardArray[randomNum])
+            }
+        }
+        console.log(updatedArray);
+    });
+
     return (
         <div>
             <section className="cardContainer"> 
-                <button className="card" onClick={setScore}></button>
-                <button className="card" onClick={setScore}></button>
-                <button className="card" onClick={setScore}></button>
-                <button className="card" onClick={setScore}></button>
-                <button className="card" onClick={setScore}></button>
-                <button className="card" onClick={setScore}></button>
-                <button className="card" onClick={setScore}></button>
-                <button className="card" onClick={setScore}></button>
-                <button className="card" onClick={setScore}></button>
-                <button className="card" onClick={setScore}></button>
-                <button className="card" onClick={setScore}></button>
-                <button className="card" onClick={setScore}></button>
+                {updatedArray.map((card) => {
+                    return <button className="card" onClick={setScore}>{card}</button>;
+                })}
             </section>
         </div>
-    )
+    );
 }
 
 export default Cards;
